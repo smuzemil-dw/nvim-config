a7ef9d824e0c0e3cf4d465f10f135175da1a175b
smuzemil <smuzemil@deepwalkresearch.com>
2024-04-25 19:47:52 -0500

    updated keymaps

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -8,18 +8,18 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Switch to top window "})
 vim.keymap.set("n", "<leader>v", "<C-w>v", { desc = "Create a vertical split window" })
 vim.keymap.set("n", "<leader>s", ":sp", { desc = "Create a horizontal split window" })
 
-vim.api.nvim_set_keymap('n', '<leader>l', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>h', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>n', ':enew<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>x', ':bd<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>l', ':BufferLineCycleNext<CR>', { desc = "Switch to the right buffer", noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>h', ':BufferLineCyclePrev<CR>', { desc = "Switch to the left buffer", noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>n', ':enew<CR>', { desc = "Create a new buffer window", noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>x', ':bd<CR>', { desc = "Close the current buffer", noremap = true, silent = true })
 
 vim.keymap.set("n", "<C-s>h", ":sb", { noremap = true, silent = true, desc = "Move buffer to new horizontal window" })
 vim.keymap.set("n", "<C-s>v", ":vert sb", { noremap = true, silent = true, desc = "Move buffer to a new vertical window" })
 
-vim.keymap.set('n', '<leader>ca', ':lua vim.lsp.buf.code_action()', { noremap = true, silent = true, desc = "LSP Code Actions" })
+vim.keymap.set('n', '<leader>ca', ':lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true, desc = "LSP Code Actions" })
 
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true, desc="Toggle nvim-tree menu"})
 
-vim.keymap.set("n", "<C-S>j", ":lua vim.diagnostic.goto_next()", { desc = "Jump to next ESLint error" })
-vim.keymap.set("n", "<C-S>k", ":lua vim.diagnostic.goto_prev()", { desc = "Go to previous ESLint error" })
-vim.keymap.set("n", "<C-S>x", ":lua vim.diagnostic.open_float()", { desc = "Expand ESLint error"})
+vim.keymap.set("n", "sj", ":lua vim.diagnostic.goto_next()<CR>", { desc = "Jump to next ESLint error", silent = true })
+vim.keymap.set("n", "sk", ":lua vim.diagnostic.goto_prev()<CR>", { desc = "Go to previous ESLint error", silent = true })
+vim.keymap.set("n", "sx", ":lua vim.diagnostic.open_float()<CR>", { desc = "Expand ESLint error", silent = true })
