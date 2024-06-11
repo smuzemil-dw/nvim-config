f951642336221b0df68510e6b789d6683388dd00
smuzemil <smuzemil@deepwalkresearch.com>
2024-06-11 10:10:04 -0500

    added undodir configs

--- lua/config.lua
+++ lua/config.lua
@@ -7,3 +7,5 @@ vim.opt.expandtab = true
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
 vim.api.nvim_set_option("clipboard","unnamed")
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
