27297dc911d143076588d8fa9edf4ee0ee8f36ca
Sonia Muzemil <soniamuzemil@Sonias-MacBook-Pro.local>
2025-11-25 15:58:27 -0600

    fix some LS errors

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -217,23 +217,18 @@ return {
         },
     },
     {
-        "jose-elias-alvarez/null-ls.nvim",
+        "nvimtools/none-ls.nvim",
         event = "BufRead",
+        dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local null_ls = require("null-ls")
             null_ls.setup({
                 sources = {
-                    null_ls.builtins.diagnostics.eslint.with({
-                        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "css", "scss", "html", "json" },
-                    }),
                     null_ls.builtins.formatting.prettierd.with({
                       condition = function(utils)
                         return utils.has_file({ ".prettierrc" })
                       end,
                     }),
-                    null_ls.builtins.code_actions.eslint.with({
-                        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "css", "scss", "html", "json" },
-                    })
                 },
                 on_attach = function(client, bufnr)
                     if client.supports_method("textDocument/formatting") then
