877097f1ad6ef4411001914c87dc5d6bf05ab63d
Sonia Muzemil <smuzemil@deepwalkresearch.com>
2025-11-26 13:38:08 -0600

    fix language server bugs

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,42 +1,33 @@
-
 local lsp = require("lsp-zero")
 
-lsp.preset("recommended")
+-- Configure mason-lspconfig to install and setup servers
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'ts_ls',       -- TypeScript/JavaScript
+    'eslint',      -- ESLint
+    'lua_ls',      -- Lua
+    'pyright',     -- Python
+  },
+  handlers = {
+    -- Default handler for all servers
+    lsp.default_setup,
 
--- Fix Undefined global 'vim'
-lsp.configure('lua-language-server', {
-    settings = {
-        Lua = {
+    -- Custom handler for lua_ls to fix 'vim' undefined warning
+    lua_ls = function()
+      require('lspconfig').lua_ls.setup({
+        settings = {
+          Lua = {
             diagnostics = {
-                globals = { 'vim' }
+              globals = { 'vim' }
             }
+          }
         }
-    }
+      })
+    end,
+  },
 })
 
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
-})
-
+-- LSP keymaps - triggered when LSP attaches to buffer
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
@@ -52,13 +43,22 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-lsp.setup()
-
+-- Diagnostic configuration
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    source = 'always',
+  },
 })
 
-vim.lsp.buf.format({
-    bufnr = bufnr,
-    timeout_ms = 5000, -- Increase the timeout to 5000 milliseconds
-})
+-- Change diagnostic symbols in the sign column
+local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
